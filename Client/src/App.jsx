7734b1cd9349f9771f2fa1d52f168d255446bb99
Jabi-0935 <shaikjabiulla981@gmail.com>
2025-09-12 00:30:33 +0530

    defining the frontend files

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import Features from "./components/Features";
+
+
+
 
 function App() {
   return (
     <>
-      <div className="flex flex-col min-h-screen bg-[#0d1216]">
+      <div className="flex flex-col min-h-screen ">
         <NavBar />
-        <Hero />
+        <Hero/>
       </div>
     </>
   );
