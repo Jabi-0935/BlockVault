78d8de24fd936acc2190966668df4eb8f6dcac06
Jabi-0935 <shaikjabiulla981@gmail.com>
2025-09-11 22:27:26 +0530

    Move Git repo to project root and add client folder

---
+++ Client/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Logo from "../assets/Logo.png";
+
+const NavBar = () => {
+  return (
+    <nav className="text-[rgb(223_229_236)] w-full h-16 border-b border-gray-300 bg-[#0d1216] px-4 py-2 box-border flex justify-between items-center sticky top-0">
+      <div className="logo flex items-center gap-2">
+        <img src={Logo} alt="BlockVault" className="h-10 w-auto" />
+        <a href="/" className="font-bold">BlockVault</a>
+      </div>
+      <div className="auths flex gap-4 font-medium">
+        <a className="px-3 py-1 border border-gray-600 rounded-xl hover:bg-gray-700 transition" href="/login">Login</a>
+        <a className="px-3 py-1 border border-gray-600 rounded-xl hover:bg-gray-700 transition" href="/signup">SignUp</a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
