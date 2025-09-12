e6aa59c1435ede734f2d079746d0f564cbe938a3
Jabi-0935 <shaikjabiulla981@gmail.com>
2025-09-13 00:39:59 +0530

    add auth page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
-import Features from "./components/Features";
+import Auth from "./pages/Auth";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router =  createBrowserRouter([
+  {
+    path:"/",
+    element:<Hero/>
+  },
+  {path:'/auth',
+    element:<Auth/>
+  }
+])
+
+
 
 
 
@@ -10,8 +23,9 @@ function App() {
   return (
     <>
       <div className="flex flex-col min-h-screen ">
-        <NavBar />
-        <Hero/>
+        <NavBar/>
+        <RouterProvider router={router}/>
+
       </div>
     </>
   );
--- Client/src/components/Hero.jsx
+++ Client/src/components/Hero.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
+  const navigate = useNavigate();
   return (
     <>
-    <div className=" p-5 text-[rgb(223_229_236)] main bg-[#0d1216] w-full border-b border-gray-300 min-h-[80vh] flex flex-col flex-grow items-center justify-center text-center">
-        <h1 className='text-3xl md:text-5xl font-bold'>Track Your Crypto Portfolio with Ease</h1>
-        <p className='text-1.5xs md:text-lg text-gray-400 mt-4 mb-4'>Get live prices, track your investments, and view analytics in one place</p>
-        <button type="button" className=" shadow-lg bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 inline-flex items-center justify-center font-semibold text-sm leading-5 rounded-lg select-focus:outline-none px-2.5 py-1.5">Get Started</button>
-    </div>
+      <div className=" p-5 w-full border-b border-gray-300 min-h-[80vh] flex flex-col flex-grow items-center justify-center text-center">
+        <h1 className="text-3xl md:text-5xl font-bold">
+          Track Your Crypto Portfolio with Ease
+        </h1>
+        <p className="text-1.5xs md:text-lg text-gray-400 mt-4 mb-4">
+          Get live prices, track your investments, and view analytics in one
+          place
+        </p>
+        <button
+          type="button"
+          className=" inline-flex items-center justify-center whitespace-nowrap font-semibold text-sm leading-5 rounded-lg select-none px-2.5 py-1.5 bg-gray-200 hover:bg-gray-300 dark:bg-moon-700 dark:hover:bg-moon-600 text-gray-900 hover:text-gray-900 focus:text-gray-900 dark:text-moon-50 dark:hover:text-moon-50 dark:focus:text-moon-50 focus:outline-none"
+          onClick={()=>navigate('/auth')}
+        >
+          Get Started
+        </button>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
