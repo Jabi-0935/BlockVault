2e34b12bb1a5bd70fcb3036a7c6b317566977dbe
Jabi-0935 <shaikjabiulla981@gmail.com>
2025-09-13 16:52:12 +0530

    improvement in dashboard and protected route

---
+++ Client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import React, { Children } from "react";
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({children}) => {
+  const {isAuthenticated} = useAuth();
+  if(!isAuthenticated){
+    return <Navigate to='/auth' replace/>
+  }
+  return children;
+};
+
+export default ProtectedRoute;
